838b5b169026af063760c7e495b593e96c095cfa
Алексей Лисенко <lisenko-av@bk.ru>
2022-05-30 22:13:02 +0300

    Задание 2.1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost_bilet = 5000;
+        int miles = cost_bilet/20;
+
+        System.out.println("Ваше количество бонусных миль: " + miles);
+
+    }
+}
