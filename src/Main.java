95b9c8b6ad86cf60a7fcaf74aa6cae1b46873e89
Алексей Лисенко <lisenko-av@bk.ru>
2022-06-01 18:42:05 +0300

    Задание 3.1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int costBilet = 5000;
-        int miles = costBilet / 20;
-
-        System.out.println("Ваше количество бонусных миль: " + miles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
